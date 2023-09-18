c717ac21e61b061632626ed86b2eb5687a1fffc6
DWood1322 <echo.arlandanaufal1322@gmail.com>
2023-09-18 09:12:23 +0700

    Update

--- app/Views/Main/signup.php
+++ app/Views/Main/signup.php
@@ -81,9 +81,9 @@
                                         </div>
 
                                         <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                            <a href="/home-tes">
-                                                <button type="button" class="btn btn-primary btn-lg">Register</button>
-                                            </a>
+                                            
+                                                <button type="submit" class="btn btn-primary btn-lg">Register</button>
+                                            
                                         </div>
 
                                     </div>
