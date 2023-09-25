690673fcf5070847feff97941b122286babe0c02
HitachiBana <inouehikaru365@gmail.com>
2023-09-25 15:39:40 +0700

    new senin 25 sep

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -37,12 +37,6 @@ $routes->get('/sign-in', 'Main\Dashboard::signin');
 $routes->post('/sign-in', 'Main\Dashboard::signin');
 $routes->get('/sign-up', 'Main\Dashboard::signup');
 $routes->post('/sign-up', 'Main\Dashboard::signup');
-$routes->get('/home', 'Main\Dashboard::main');
+$routes->get('/home', 'Main\Dashboard::home');
 $routes->get('/home/page', 'Main\Dashboard::page');
 $routes->post('/logout', 'Main\Dashboard::logout');
-$routes->get('/home-tes', 'Main\Dashboard::home');
-
-
-
-
-
